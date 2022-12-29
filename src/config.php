155a6879e21ebff155763dbaf53862b8e38566a1
ehsan9 <h.ehsan24@gmail.com>
2022-12-29 14:55:18 +0330

    feat: make config of metabase

---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'url' => 'https://yoursmetabase.com',
+    'username' => 'your_metabase_username',
+    'password' => 'your_metabase_pass',
+    'is_make_when_bind_app' => false,
+];
